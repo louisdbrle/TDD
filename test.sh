05e85c103007990d218183491c5dec7e2680d708
Louis <louisdubarle@free.fr>
2024-01-07 15:19:42 +0100

    sh typo

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TEST_RUNNER=${PROJECT_DIR}/funcs/tests/funcs_test_main.py
+TEST_RUNNER=${PROJECT_DIR}/funcs/tests/funcs_tests_main.py
 MODULE_DIR=${PROJECT_DIR}/funcs
 
 echo "Testing funcs"
@@ -10,7 +10,7 @@ python3 -m unittest ${TEST_RUNNER}
 
 echo "Done testing funcs"
 
-TEST_RUNNER=${PROJECT_DIR}/structs/tests/structs_test_main.py
+TEST_RUNNER=${PROJECT_DIR}/structs/tests/structs_tests_main.py
 MODULE_DIR=${PROJECT_DIR}/structs
 
 echo "Testing structs"
