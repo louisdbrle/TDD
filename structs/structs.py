b0923b5ed8dd0299c0bf821209f2db30fdfa4ec0
Louis <louisdubarle@free.fr>
2024-01-07 16:22:43 +0100

    LIFO done / tested

--- structs/structs.py
+++ structs/structs.py
@@ -22,7 +22,26 @@ class FIFO:
 
 
 class LIFO:
-    pass
+    def __init__(self):
+        self.first = [None, None]
+        self.last = [None, None]
+
+    def push(self, value):
+        el = [value, None]
+        if self.first[0] is None:
+            self.first = el
+            self.last = el
+        else:
+            el[1] = self.last
+            self.last = el
+
+    def pop(self):
+        if self.last is None:
+            return None
+        else:
+            value = self.last[0]
+            self.last = self.last[1]
+            return value
 
 
 class LILO:
--- structs/tests/structs_tests_main.py
+++ structs/tests/structs_tests_main.py
@@ -1,5 +1,6 @@
 import unittest
 from .test_fifo import TestFIFO
+from .test_lifo import TestLIFO
 
 if __name__ == "__main__":
     unittest.main()
