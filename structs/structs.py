85c0a77bdece2b40c340edd9b87cb5ef5fe3bcef
Louis <louisdubarle@free.fr>
2024-01-07 16:19:37 +0100

    FIFO done / tested

--- structs/structs.py
+++ structs/structs.py
@@ -1,5 +1,24 @@
 class FIFO:
-    pass
+    def __init__(self):
+        self.first = [None, None]
+        self.last = [None, None]
+
+    def push(self, value):
+        el = [value, None]
+        if self.first[0] is None:
+            self.first = el
+            self.last = el
+        else:
+            self.last[1] = el
+            self.last = el
+
+    def pop(self):
+        if self.first is None:
+            return None
+        else:
+            value = self.first[0]
+            self.first = self.first[1]
+            return value
 
 
 class LIFO:
