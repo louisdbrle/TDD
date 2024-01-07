cfdc18a8ee09e44a9ad7d56ce87d7eb1fe3763e8
Louis <louisdubarle@free.fr>
2024-01-07 16:15:04 +0100

    changed FIFO tests

--- structs/tests/test_fifo.py
+++ structs/tests/test_fifo.py
@@ -6,14 +6,14 @@ class TestFIFO(unittest.TestCase):
     def test_push(self):
         self.fifo = FIFO()
         self.fifo.push(1)
-        self.assertEqual(self.fifo.last, 1)
-        self.assertEqual(self.fifo.first, 1)
+        self.assertEqual(self.fifo.last[0], 1)
+        self.assertEqual(self.fifo.first[0], 1)
         self.fifo.push(2)
-        self.assertEqual(self.fifo.last, 2)
-        self.assertEqual(self.fifo.first, 1)
+        self.assertEqual(self.fifo.last[0], 2)
+        self.assertEqual(self.fifo.first[0], 1)
         self.fifo.push(3)
-        self.assertEqual(self.fifo.last, 3)
-        self.assertEqual(self.fifo.first, 1)
+        self.assertEqual(self.fifo.last[0], 3)
+        self.assertEqual(self.fifo.first[0], 1)
 
     def test_pop(self):
         self.fifo = FIFO()
