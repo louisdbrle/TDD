e26e3a3a859322a67f10c739d2f85d5994f81c75
Louis <louisdubarle@free.fr>
2024-01-13 11:15:26 +0100

    added tests

--- bstpy/tests/bstpy_tests_main.py
+++ bstpy/tests/bstpy_tests_main.py
@@ -16,12 +16,25 @@ class TestBST(unittest.TestCase):
         self.bst.insert(5)
         self.bst.insert(15)
         self.assertEqual(self.bst.height(), 1)
+        self.bst.insert(20)
+        self.assertEqual(self.bst.height(), 2)
+
+    def test_get_min_value(self):
+        self.bst.insert(5)
+        self.bst.insert(15)
+        self.assertEqual(self.bst.get_min_value(), 5)
+        self.bst.insert(20)
+        self.assertEqual(self.bst.get_min_value(), 5)
+        self.bst.insert(2)
+        self.assertEqual(self.bst.get_min_value(), 2)
 
     def test_delete(self):
         self.bst.insert(5)
         self.bst.insert(15)
         self.bst.delete(5)
         self.assertEqual(self.bst.left, None)
+        self.bst.delete(15)
+        self.assertEqual(self.bst.right, None)
 
 
 if __name__ == "__main__":
